7c1f9cb1e5e8e6a3f19e50d212e3bde9fe9036f5
gmalmsten <gustavmalmsten@gmail.com>
2023-04-16 15:17:59 +0200

    Changed partitioning scheme to A-row, B-Column and started experimenting with BLAS

--- A2/matmul.c
+++ A2/matmul.c
@@ -54,21 +54,34 @@ int main(int argc, char *argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     // Create cartesian grid virtual topology
+	// MPI_Comm GRID_COMM;
+	// int dims[2];
+	// int periods[2];
+	// int reorder = 0;  // May use reorder and MPI_Cart_rank(GRID_COMM, coords[], &rank);
+	// dims[0] = sqrt(num_proc);
+    // dims[1] = sqrt(num_proc);
+	// periods[0] = 0;
+    // periods[1] = 0;
+
+	// MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &GRID_COMM);
+
+    // // Check that the processes have the correct coords
+    // int coords[2];
+    // MPI_Cart_coords(GRID_COMM, rank, 2, coords);
+    // printf("Rank %d coords (%d, %d)\n", rank, coords[0], coords[1]);
+
+
+    // Create periodic virtual topology
 	MPI_Comm GRID_COMM;
-	int dims[2];
-	int periods[2];
-	int reorder = 0;  // May use reorder and MPI_Cart_rank(GRID_COMM, coords[], &rank);
-	dims[0] = sqrt(num_proc);
-    dims[1] = sqrt(num_proc);
+	int dims[1];
+	int periods[1];
+	int reorder = 0;  
+	dims[0] = num_proc;
 	periods[0] = 0;
-    periods[1] = 0;
 
-	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &GRID_COMM);
+	MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, reorder, &GRID_COMM);
 
-    // Check that the processes have the correct coords
-    int coords[2];
-    MPI_Cart_coords(GRID_COMM, rank, 2, coords);
-    printf("Rank %d coords (%d, %d)\n", rank, coords[0], coords[1]);
+
     // Initialize matrices
     double *A, *B, *C, *input;
     int n, m;
@@ -94,6 +107,24 @@ int main(int argc, char *argv[]){
         }
         printf("\n");
     }
+
+    // Transpose B matrix to send rows using scatter
+    for(int i = 0; i < n; i++)
+        for(int j = i + 1; j < n; j++){
+            double temp = input[n*n + n*i + j];
+            input[n*n + n*i + j] = input[n*n + n*j + i];
+            input[n*n + n*j + i] = temp;
+        }
+
+    printf("\n");
+    printf("B': \n");
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            printf("%lf ", input[n*n + n*i + j]);
+        }
+        printf("\n");
+    }
+    
     }
 
     // Broadcast set up parameters
@@ -110,27 +141,45 @@ int main(int argc, char *argv[]){
 
 
     // Scatter matrices to processes
-    A = (double *)malloc((n*m)*sizeof(double));
+    A = (double *)malloc((m*n)*sizeof(double));
     B = (double *)malloc((n*m)*sizeof(double));
     MPI_Scatter(input, 1, matrixtype, A, n*m, MPI_DOUBLE, 0, GRID_COMM);
     MPI_Scatter(&input[n*n], 1, matrixtype, B, n*m, MPI_DOUBLE, 0, GRID_COMM);
 
 
-    // Print submatrices as rows
-    // printf("Rank %d ", rank);
-    // for(int i = 0; i < n*n/(num_proc); i++){
-    //     printf("%lf ", A[i]);
-    // }
-    // printf("\n");
+    // // Print submatrices as rows
 
-    //     printf("Rank %d ", rank);
-    // for(int i = 0; i < n*n/(num_proc); i++){
-    //     printf("%lf ", B[i]);
-    // }
-    // printf("\n");
 
     // Allocate memory for local result
-    C = (double *)malloc(n*m*sizeof(double));
+    C = (double *)malloc(m*m*sizeof(double));
+    if(rank==0){
+        printf("m: %d n: %d\n", m, n);
+    for(int i = 0; i < m; i++){
+    for(int j = 0; j < n; j++){
+        printf("%lf ", A[i*n + j]);
+    }
+    printf("\n");
+    }
+    printf("\n");
+
+    for(int i = 0; i < n; i++){
+    for(int j = 0; j<m; j++){
+        printf("%lf ", B[i + j*n]);
+    }
+    printf("\n");
+    }
+    printf("\n");
+        double *CTEST = (double *)malloc(m*m*sizeof(double));
+        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, m, n, 1.0, A, n, B, m, 0.0, CTEST, m);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < m; j++){
+                printf("%lf ",CTEST[i*m + j]);
+            }
+            printf("\n");
+        }
+        
+        free(CTEST);
+    }
     
     MPI_Type_free(&matrixtype);
     MPI_Finalize();
